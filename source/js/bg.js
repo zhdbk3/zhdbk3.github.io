14ce044ea5ba4346c733b862c41c7e50f397306e
着火的冰块nya <zhdbk3@outlook.com>
2025-11-16 15:05:06 +0800

    修复副标题引号 bug | IIFE | 调整 prettier

--- source/js/bg.js
+++ source/js/bg.js
@@ -1,14 +1,14 @@
-const urls = [
-  '/img/bg/mahiro_1.avif',
-  '/img/bg/mahiro_2.avif',
-  '/img/bg/mahiro_3.avif',
-  '/img/bg/nachoneko_1.avif',
-];
+(function () {
+  const urls = [
+    '/img/bg/mahiro_1.avif',
+    '/img/bg/mahiro_2.avif',
+    '/img/bg/mahiro_3.avif',
+    '/img/bg/nachoneko_1.avif',
+  ];
 
-// 随机抽取一个 URL
-const i = Math.floor(Math.random() * urls.length);
-const url = urls[i];
+  // 随机抽取一个 URL
+  const i = Math.floor(Math.random() * urls.length);
+  const url = urls[i];
 
-addEventListener('DOMContentLoaded', () => {
   document.getElementById('web_bg').style.backgroundImage = `url(${url})`;
-});
+})();
--- source/js/quotes.js
+++ source/js/quotes.js
@@ -1,54 +1,58 @@
-/**
- * 当前是否为直角引号模式
- * @type {boolean}
- */
-let squareQuotes = localStorage.getItem('squareQuotes') === 'true';
+(function () {
+  /**
+   * 当前是否为直角引号模式
+   * @type {boolean}
+   */
+  let squareQuotes = localStorage.getItem('squareQuotes') === 'true';
 
-const charPairs = [
-  ['“', '「'],
-  ['”', '」'],
-  ['‘', '『'],
-  ['’', '』'],
-];
+  const charPairs = [
+    ['“', '「'],
+    ['”', '」'],
+    ['‘', '『'],
+    ['’', '』'],
+  ];
 
-/**
- * 切换一个元素的引号
- * @param el {HTMLElement}
- */
-function toggleQuotesInAnElement(el) {
-  let text = el.innerHTML; // 使用 innerHTML 而不是 innerText，以避免破坏原有结构
-  for (const pair of charPairs) {
-    if (squareQuotes) {
-      text = text.replaceAll(pair[0], pair[1]);
-    } else {
-      text = text.replaceAll(pair[1], pair[0]);
+  /**
+   * 切换一个元素的引号
+   * @param el {HTMLElement}
+   */
+  function toggleQuotesInAnElement(el) {
+    let text = el.innerHTML; // 使用 innerHTML 而不是 innerText，以避免破坏原有结构
+    for (const pair of charPairs) {
+      if (squareQuotes) {
+        text = text.replaceAll(pair[0], pair[1]);
+      } else {
+        text = text.replaceAll(pair[1], pair[0]);
+      }
     }
+    el.innerHTML = text;
   }
-  el.innerHTML = text;
-}
 
-/**
- * 切换整个页面所有合适元素内的引号
- */
-function toggleQuotes() {
-  squareQuotes = !squareQuotes;
-  localStorage.setItem('squareQuotes', squareQuotes.toString());
-  document
-    .querySelectorAll(
-      'a.article-title, div.content, h1.post-title, ol.toc, article p, article li, ' +
-        'article h1, article h2, article h3, article h4, article h5, article h6',
-      // 标题的 id 不应被替换，故采用这种写法
-      // 右侧导航（ol.toc）内锚点的特殊字符均被转义，故不需要考虑
-    )
-    .forEach(toggleQuotesInAnElement);
-}
+  /**
+   * 切换整个页面所有合适元素内的引号
+   */
+  function toggleQuotes() {
+    squareQuotes = !squareQuotes;
+    localStorage.setItem('squareQuotes', squareQuotes.toString());
+    document
+      .querySelectorAll(
+        'div#site-subtitle, a.article-title, div.content, h1.post-title, ol.toc, article p, article li, ' +
+          'article h1, article h2, article h3, article h4, article h5, article h6',
+        // 标题的 id 不应被替换，故采用这种写法
+        // 右侧导航（ol.toc）内锚点的特殊字符均被转义，故不需要考虑
+      )
+      .forEach(toggleQuotesInAnElement);
+  }
 
-// 添加自定义右下角按钮
-document.getElementById('rightside-config-show').innerHTML +=
-  '<button type="button" title="“弯引号”和「直角引号」切换" onclick="toggleQuotes()">' +
-  '<i class="fas fa-quote-left"></i>' +
-  '</button>';
+  // 添加自定义右下角按钮
+  const btn = document.createElement('button');
+  btn.type = 'button';
+  btn.title = '“弯引号”和「直角引号」切换';
+  btn.innerHTML = '<i class="fas fa-quote-left"></i>';
+  btn.addEventListener('click', toggleQuotes);
+  document.getElementById('rightside-config-show').appendChild(btn);
 
-// 立即执行一次
-squareQuotes = !squareQuotes; // 抵消反转状态
-toggleQuotes();
+  // 立即执行一次
+  squareQuotes = !squareQuotes; // 抵消反转状态
+  toggleQuotes();
+})();
